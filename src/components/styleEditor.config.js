7784a8ea7cd195fb33fca5c5e00549ed6562ce6d
gavinjhyang <gavinjhyang@tencent.com>
2021-02-04 02:18:17 +0800

    text

--- src/components/styleEditor.config.js
+++ src/components/styleEditor.config.js
@@ -287,78 +287,397 @@
     key: 'text',
     label: '文本属性',
     group: [{
-        key: '',
-        component: '',
+        key: 'color',
+        component: 'color',
         options: [],
-        tip: ``,
-        label: ''
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+            <td><i>color_name</i></td>
+            <td>规定颜色值为颜色名称的颜色（比如 red）。</td>
+        </tr>
+        
+        <tr>
+            <td><i>hex_number</i></td>
+            <td>规定颜色值为十六进制值的颜色（比如 #ff0000）。</td>
+        </tr>
+        
+        <tr>
+            <td><i>rgb_number</i></td>
+            <td>规定颜色值为 rgb 代码的颜色（比如 rgb(255,0,0)）。</td>
+        </tr>
+        
+        <tr>
+            <td>inherit</td>
+            <td>规定应该从父元素继承颜色。</td>
+        </tr>
+        </tbody></table>`,
+        label: '文本颜色'
     }, {
-        key: '',
-        component: '',
+        key: 'direction',
+        component: 'select',
+        options: ['ltr', 'rtl', 'inherit'],
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>ltr</td>
+        <td>默认。文本方向从左到右。</td>
+        </tr>
+        
+        <tr>
+        <td>rtl</td>
+        <td>文本方向从右到左。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 direction 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '文本方向'
+    }, {
+        key: 'line-height',
+        component: 'input',
         options: [],
-        tip: ``,
-        label: ''
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>normal</td>
+        <td>默认。设置合理的行间距。</td>
+        </tr>
+        
+        <tr>
+        <td><i>number</i></td>
+        <td>设置数字，此数字会与当前的字体尺寸相乘来设置行间距。</td>
+        </tr>
+        
+        <tr>
+        <td><i>length</i></td>
+        <td>设置固定的行间距。</td>
+        </tr>
+        
+        <tr>
+        <td><i>%</i></td>
+        <td>基于当前字体尺寸的百分比行间距。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 line-height 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '行高'
     }, {
-        key: '',
-        component: '',
+        key: 'letter-spacing',
+        component: 'input',
         options: [],
-        tip: ``,
-        label: ''
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>normal</td>
+        <td>默认。规定字符间没有额外的空间。</td>
+        </tr>
+        
+        <tr>
+        <td><i>length</i></td>
+        <td>定义字符间的固定空间（允许使用负值）。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 letter-spacing 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '字符间距'
     }, {
-        key: '',
-        component: '',
+        key: 'text-align',
+        component: 'select',
+        options: ['left', 'right', 'center', 'justify', 'inherit'],
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>left</td>
+        <td>把文本排列到左边。默认值：由浏览器决定。</td>
+        </tr>
+        
+        <tr>
+        <td>right</td>
+        <td>把文本排列到右边。</td>
+        </tr>
+        
+        <tr>
+        <td>center</td>
+        <td>把文本排列到中间。</td>
+        </tr>
+        
+        <tr>
+        <td>justify</td>
+        <td>实现两端对齐文本效果。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 text-align 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '对齐方式'
+    }, {
+        key: 'text-decoration',
+        component: 'select',
+        options: ['none', 'underline', 'overline', 'line-through', 'blink', 'inherit'],
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>none</td>
+        <td>默认。定义标准的文本。</td>
+        </tr>
+        
+        <tr>
+        <td>underline</td>
+        <td>定义文本下的一条线。</td>
+        </tr>
+        
+        <tr>
+        <td>overline</td>
+        <td>定义文本上的一条线。</td>
+        </tr>
+        
+        <tr>
+        <td>line-through</td>
+        <td>定义穿过文本下的一条线。</td>
+        </tr>
+        
+        <tr>
+        <td>blink</td>
+        <td>定义闪烁的文本。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 text-decoration 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '文本修饰'
+    }, {
+        key: 'text-indent',
+        component: 'input',
         options: [],
-        tip: ``,
-        label: ''
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td><i>length</i></td>
+        <td>定义固定的缩进。默认值：0。</td>
+        </tr>
+        
+        <tr>
+        <td><i>%</i></td>
+        <td>定义基于父元素宽度的百分比的缩进。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 text-indent 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '缩进'
     }, {
-        key: '',
-        component: '',
+        key: 'text-shadow',
+        component: 'input',
         options: [],
-        tip: ``,
-        label: ''
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th style="width:25%;">值</th>
+        <th>描述</th>
+        <th style="width:8%;">测试</th>
+        </tr>
+        
+        <tr>
+        <td><i>h-shadow</i></td>
+        <td>必需。水平阴影的位置。允许负值。</td>
+        <td><a target="_blank" href="/tiy/c.asp?f=css_text-shadow">测试</a></td>
+        </tr>
+        
+        <tr>
+        <td><i>v-shadow</i></td>
+        <td>必需。垂直阴影的位置。允许负值。</td>
+        <td><a target="_blank" href="/tiy/c.asp?f=css_text-shadow">测试</a></td>
+        </tr>
+        
+        <tr>
+        <td><i>blur</i></td>
+        <td>可选。模糊的距离。</td>
+        <td><a target="_blank" href="/tiy/c.asp?f=css_text-shadow&amp;p=11">测试</a></td>
+        </tr>
+        
+        <tr>
+        <td><i>color</i></td>
+        <td>可选。阴影的颜色。参阅 <a href="/cssref/css_colors_legal.asp" title="CSS 合法颜色值">CSS 颜色值</a>。</td>
+        <td><a target="_blank" href="/tiy/c.asp?f=css_text-shadow&amp;p=13">测试</a></td>
+        </tr>
+        </tbody></table>`,
+        label: '文本阴影'
     }, {
-        key: '',
-        component: '',
+        key: 'text-transform',
+        component: 'select',
+        options: ['none', 'capitalize', 'uppercase', 'lowercase', 'inherit'],
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>none</td>
+        <td>默认。定义带有小写字母和大写字母的标准的文本。</td>
+        </tr>
+        
+        <tr>
+        <td>capitalize</td>
+        <td>文本中的每个单词以大写字母开头。</td>
+        </tr>
+        
+        <tr>
+        <td>uppercase</td>
+        <td>定义仅有大写字母。</td>
+        </tr>
+        
+        <tr>
+        <td>lowercase</td>
+        <td>定义无大写字母，仅有小写字母。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 text-transform 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '控制字母'
+    }, {
+        key: 'unicode-bidi',
+        component: 'select',
+        options: ['normal', 'embed', 'bidi-override'],
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>normal</td>
+        <td>元素不会对双向算法打开附加的一层嵌套。对于行内元素，顺序的隐式重排会跨元素边界进行。</td>
+        </tr>
+        
+        <tr>
+        <td>embed</td>
+        <td>如果是一个行内元素，这个值对于双向算法会打开附件的一层嵌套。这个嵌套层的方向由 direction 属性指定。会在元素内部隐式地完成顺序重排。这对应于在元素开始处增加一个 LRE（对于 direction:ltr ：U+202A）或 RLE（对于 direction:rtl ：U+202B），并在元素的最后增加一个 PDF（U+202C）。</td>
+        </tr>
+        
+        <tr>
+        <td>bidi-override</td>
+        <td>这会为行内元素创建一个覆盖。对于块级元素，将为不在另一块中的行内后代创建一个覆盖。这说明，顺序重排在元素内部严格按照 direction 属性进行；忽略了双向算法的隐式部分。这对应于在元素开始处增加一个 LRO（对于 direction:ltr ：U+202D）或 RLO（对于 direction:rtl ：U+202E），并在元素最后增加一个 PDF（U+202C）。</td>
+        </tr>
+        </tbody></table>`,
+        label: '文本方向'
+    }, {
+        key: 'white-space',
+        component: 'select',
+        options: ['normal', 'pre', 'nowrap', 'pre-wrap', 'pre-line', 'inherit'],
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>normal</td>
+        <td>默认。空白会被浏览器忽略。</td>
+        </tr>
+        
+        <tr>
+        <td>pre</td>
+        <td>空白会被浏览器保留。其行为方式类似 HTML 中的 &lt;pre&gt; 标签。</td>
+        </tr>
+        
+        <tr>
+        <td>nowrap</td>
+        <td>文本不会换行，文本会在在同一行上继续，直到遇到 &lt;br&gt; 标签为止。</td>
+        </tr>
+        
+        <tr>
+        <td>pre-wrap</td>
+        <td>保留空白符序列，但是正常地进行换行。</td>
+        </tr>
+        
+        <tr>
+        <td>pre-line</td>
+        <td>合并空白符序列，但是保留换行符。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 white-space 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '元素空白'
+    }, {
+        key: 'word-spacing',
+        component: 'input',
         options: [],
-        tip: ``,
-        label: ''
-    }, {
-        key: '',
-        component: '',
-        options: [],
-        tip: ``,
-        label: ''
-    }, {
-        key: '',
-        component: '',
-        options: [],
-        tip: ``,
-        label: ''
-    }, {
-        key: '',
-        component: '',
-        options: [],
-        tip: ``,
-        label: ''
-    }, {
-        key: '',
-        component: '',
-        options: [],
-        tip: ``,
-        label: ''
-    }, {
-        key: '',
-        component: '',
-        options: [],
-        tip: ``,
-        label: ''
-    }, {
-        key: '',
-        component: '',
-        options: [],
-        tip: ``,
-        label: ''
-    }, {
+        tip: `<table class="dataintable">
+        <tbody><tr>
+        <th>值</th>
+        <th>描述</th>
+        </tr>
+        
+        <tr>
+        <td>normal</td>
+        <td>默认。定义单词间的标准空间。</td>
+        </tr>
+        
+        <tr>
+        <td><i>length</i></td>
+        <td>定义单词间的固定空间。</td>
+        </tr>
+        
+        <tr>
+        <td>inherit</td>
+        <td>规定应该从父元素继承 word-spacing 属性的值。</td>
+        </tr>
+        </tbody></table>`,
+        label: '字间距'
+    }]
+}, {
+    key: 'text',
+    label: '文本属性',
+    group: [{
         key: '',
         component: '',
         options: [],
