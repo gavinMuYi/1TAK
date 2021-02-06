657afe570fe2edf876883c4758a7759b92dd9af7
gavinMuYi <1446623432@qq.com>
2021-02-06 23:57:04 +0800

    hahs

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,5 +1,9 @@
 var createHash = function (hashLength) {
-    return Array.from(Array(Number(hashLength) || 24), () => Math.floor(Math.random() * 36).toString(36)).join('');
+    var hash = Array.from(Array(Number(hashLength) || 24), () => Math.floor(Math.random() * 36).toString(36)).join('');
+    do {
+        hash = Array.from(Array(Number(hashLength) || 24), () => Math.floor(Math.random() * 36).toString(36)).join('');
+    } while (!isNaN(hash[0]));
+    return hash;
 };
 
 export {
