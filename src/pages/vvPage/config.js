f58a4830d94e116a4da6bedc2547c39a7ff033ec
gavinjhyang <gavinjhyang@tencent.com>
2021-04-09 18:38:24 +0800

    G_TEST

--- src/pages/vvPage/config.js
+++ src/pages/vvPage/config.js
@@ -1,12 +1,12 @@
 exports.iconCompMap = {
-    'icon-shurukuang': 'G_INPUT',
-    'icon-yuanliao': 'G_TEST'
+    'icon-yuanliao': 'G_TEST',
+    'icon-shurukuang': 'G_INPUT'
 };
 
 exports.unitCompIcons = [{
-    key: 'icon-shurukuang',
-    name: '输入框'
-}, {
     key: 'icon-yuanliao',
     name: '测试组件'
+}, {
+    key: 'icon-shurukuang',
+    name: '输入框'
 }];
